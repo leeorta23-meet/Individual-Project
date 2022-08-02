f0f5cc87a3af1dafe09a160c0eccf5a4a97d99be
leeorta23-meet <109899549+leeorta23-meet@users.noreply.github.com>
2022-08-02 17:55:22 +0300

    s

--- Individual-Project/app.py
+++ Individual-Project/app.py
@@ -6,6 +6,10 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
 #Code goes below here
+@app.route('/', methods=['GET', 'POST'])
+def home():
+       
+        return render_template("home.html")
 
 
 
